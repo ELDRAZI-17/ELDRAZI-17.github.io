52f34c2202b3c79b5340e23c8f217bfc6d4658bd
ELDRAZI-17 <78639467+ELDRAZI-17@users.noreply.github.com>
2021-04-25 20:38:52 +0200

    Update what.rb

--- what/what.rb
+++ what/what.rb
@@ -1,3 +1,10 @@
 Alors en fait, le seul principe de cette page, c'est qu'elle se rafraîchi tout le temps. 😅
-Sauf que tu as trouvé le truc. 😮 Tu as appuyer sur la petite croix ! Ouaaaaaaaiiiiiiiis ! 😂
-La page a arrêté de se rafraîchir. Tu as cliquer sur "what what what what" et tu es ensuite arrivé ici... 😶
+Sauf que tu as trouvé le truc. 😮
+Tu as appuyer sur la petite croix ! 😱
+Ouaaaaaaaiiiiiiiis ! 😂
+La page a arrêté de se rafraîchir. 😃
+Tu as cliquer sur "what what what what" et tu es ensuite arrivé ici... 😶
+Tout ça pour ça. 😏
+J'espère que tu as réussi à supprimer le texte dans le cadre blanc.
+C'était ça le principe à la base... 😑
+Mais bon...
