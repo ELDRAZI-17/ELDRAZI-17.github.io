96db80b4b029872f71eef7214587e4a9c6060471
ELDRAZI-17 <78639467+ELDRAZI-17@users.noreply.github.com>
2021-04-25 19:20:18 +0200

    Create texte.rb

---
+++ texte.rb
@@ -0,0 +1,4 @@
+Pour faciliter l'écriture des formulaires dans les vues et permettre une interaction facile avec les modèles, Rails a prévu un ensemble de méthodes qui s'utilisent directement dans les vues. Il est bien entendu que nous pouvons utiliser sans problème les codes Html correspondants. Rails offre cependant quelques facilités non négligeables, notamment concernant les dates.
+
+II. Balise Input▲
+Nous allons passer en revue la plupart des balises 'Input'. Par défaut un tag Rails 'Input', mettra en évidence un attribut size=« 30 », comme j'ai généré un code source à partir d'une vue rhtml, d'office le code html, reprendra cet attribut.
