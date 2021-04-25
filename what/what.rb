dda9c61c5a67c3fc50ee8d030fa7ef5a8ad40e9e
ELDRAZI-17 <78639467+ELDRAZI-17@users.noreply.github.com>
2021-04-25 21:19:50 +0200

    Update what.rb

--- what/what.rb
+++ what/what.rb
@@ -1,16 +1,30 @@
 Alors en fait, le seul principe de la page précédente c'est de se rafraîchir tout le temps. 😅
+
 Sauf que tu as trouvé le truc. 😮
+
 Tu as appuyer sur la petite croix ! 😱
+
 Ouaaaaaaaiiiiiiiis ! 😂
+
 La page a arrêté de se rafraîchir ! 😃
+ 
 Tu as ensuite cliquer sur "what what what what" et tu es arrivé ici... 😶
+ 
 Tout ça pour ça. 😏
+ 
 J'espère que tu as réussi à supprimer le texte dans le cadre blanc.
+ 
 C'était ça le principe à la base... 😑
+ 
 Mais bon...
+ 
 Après tu peux partager le lien de la page, https://eldrazi-17.github.io/what/, pour voir si les autres sont assez intelligent pour trouver le truc aussi. 😁
+ 
 Allez salut ! 😉
+ 
+ 
 Si tu veux je met ça pour toi :
+ 
 ░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄
 ░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄
 ░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█
@@ -26,3 +40,4 @@ Si tu veux je met ça pour toi :
 ░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░█
 ░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░█
 ░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░█
+                                            
